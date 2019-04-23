71e5241a93fe4564e0363710e16ffd24154e70cf
Tony Lea <tony@tonys-mbp-2.home>
2019-04-23 13:39:31 -0700

    initial commit

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'baseUrl' => '',
+    'production' => false,
+    'siteName' => 'Block JS',
+    'siteDescription' => 'The Web Editor of your Dreams',
+
+    // Algolia DocSearch credentials
+    'docsearchApiKey' => '',
+    'docsearchIndexName' => '',
+
+    // navigation menu
+    'navigation' => require_once('navigation.php'),
+
+    // helpers
+    'isActive' => function ($page, $path) {
+        return ends_with(trimPath($page->getPath()), trimPath($path));
+    },
+    'isActiveParent' => function ($page, $menuItem) {
+        if (is_object($menuItem) && $menuItem->children) {
+            return $menuItem->children->contains(function ($child) use ($page) {
+                return trimPath($page->getPath()) == trimPath($child);
+            });
+        }
+    },
+    'url' => function ($page, $path) {
+        return starts_with($path, 'http') ? $path : '/' . trimPath($path);
+    },
+];
